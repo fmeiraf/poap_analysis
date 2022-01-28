3bc4f957f880a435b3b7d9b251cab33b2da28c27
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2022-01-27 19:20:15 -0700

    added functions to optimize the number of getLogs

--- data_gather/utils.py
+++ data_gather/utils.py
@@ -1,3 +1,5 @@
+import enum
+from numpy import block
 import requests
 import json
 import time
@@ -72,6 +74,7 @@ def extract_all_tokens_from_subgraph(
 # Credits to Mikko Ohtamaa on : https://ethereum.stackexchange.com/questions/51637/get-all-the-past-events-of-the-contract
 # Thanks for the awesome code dude! :)
 def fetch_events(
+    latest_block_number,
     event,
     argument_filters=None,
     from_block=None,
@@ -103,24 +106,72 @@ def fetch_events(
     argument_filters = dict()
     _filters = dict(**argument_filters)
 
-    data_filter_set, event_filter_params = construct_event_filter_params(
-        abi,
-        abi_codec,
-        contract_address=event.address,
-        argument_filters=_filters,
-        fromBlock=from_block,
-        toBlock=to_block,
-        address=address,
-        topics=topics,
-    )
+    # data_filter_set, event_filter_params = construct_event_filter_params(
+    #     abi,
+    #     abi_codec,
+    #     contract_address=event.address,
+    #     argument_filters=_filters,
+    #     fromBlock=from_block,
+    #     toBlock=to_block,
+    #     address=address,
+    #     topics=topics,
+    # )
 
-    # Call node over JSON-RPC API
-    logs = event.web3.eth.getLogs(event_filter_params)
+    # # Call node over JSON-RPC API
+    # logs = event.web3.eth.getLogs(event_filter_params)
+
+    logs = find_optimal_params_for_getLogs(
+        abi=abi,
+        abi_codec=abi_codec,
+        topics=topics,
+        event=event,
+        filters=_filters,
+        address=address,
+        latest_block_number=latest_block_number,
+    )
 
     # Convert raw binary event data to easily manipulable Python objects
     for entry in logs:
         data = get_event_data(abi_codec, abi, entry)
         yield data
+
+
+def find_optimal_params_for_getLogs(
+    abi, abi_codec, topics, event, filters, address, latest_block_number
+):
+    split_in = 2
+    while True:
+        print(f"Trying to fetch logs using n={split_in}")
+        all_logs = []
+        increment = round(latest_block_number / split_in)
+        block_num_splits = [increment * n for n in range(1, split_in + 1)]
+
+        # is_one_number_list = True if len(block_num_splits) == 1 else False
+
+        for index, block_num in enumerate(block_num_splits):
+            from_block = 0 if index == 0 else block_num_splits[index - 1] + 1
+            to_block = block_num
+
+            data_filter_set, event_filter_params = construct_event_filter_params(
+                abi,
+                abi_codec,
+                contract_address=event.address,
+                argument_filters=filters,
+                fromBlock=from_block,
+                toBlock=to_block,
+                address=address,
+                topics=topics,
+            )
+
+            try:
+                logs = event.web3.eth.getLogs(event_filter_params)
+                all_logs.extend(logs)
+            except ValueError:
+                split_in += 1
+                break
+
+            if index == len(block_num_splits) - 1:
+                return all_logs
 
 
 def fetch_log_history(contract: web3.contract):
@@ -133,9 +184,12 @@ def fetch_log_history(contract: web3.contract):
     return event_data
 
 
-def fetch_transfer_logs(contract: web3.contract):
-    """Fetch all trading pairs on Uniswap"""
-    events = list(fetch_events(contract.events.Transfer, from_block=0))
+def fetch_transfer_logs(web3_provider: web3.Web3, contract: web3.contract):
+    """Fetch all tranfer logs from ERC 20 contract"""
+    latest_block = web3_provider.eth.get_block("latest")
+    events = list(
+        fetch_events(latest_block["number"], contract.events.Transfer, from_block=0)
+    )
     print("Got all event history.", len(events), "events")
 
     event_data = [ev["args"] for ev in events]
