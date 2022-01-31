32b606e29e8f4b38f6ca8f09890f351ae46c50a6
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2022-01-30 18:34:23 -0700

    fixed select portion, extra comma

--- data_gather/chainverse_db_queries/dao_member_daohaus.sql
+++ data_gather/chainverse_db_queries/dao_member_daohaus.sql
@@ -3,7 +3,7 @@ SELECT 	memb.*,
 		dao.address dao_address,
 		dao.inactive dao_inactive,
 		dao.created_at dao_created_at,
-		dao.chain dao_chain,
+		dao.chain dao_chain
 FROM daohaus.member memb
   INNER JOIN daohaus.dao dao ON (memb.dao_id = dao.id)
 ORDER BY memb.address
