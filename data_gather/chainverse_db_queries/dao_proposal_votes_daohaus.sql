ac51ea907e66b4916e8eb385f7c108fd8173d4eb
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2022-01-31 21:50:52 -0700

    added more info about daos and vote types

--- data_gather/chainverse_db_queries/dao_proposal_votes_daohaus.sql
+++ data_gather/chainverse_db_queries/dao_proposal_votes_daohaus.sql
@@ -1,6 +1,8 @@
 SELECT 	prop.*,
+		vt.name vote_type,
 		memb.address member_address
 FROM daohaus.proposal_votes prop
 	INNER JOIN daohaus.member memb ON (prop.member_id = memb.id)
+	INNER JOIN daohaus.vote_type vt ON (vt.id = prop.vote_type_id)
 
 		
--- data_gather/chainverse_db_queries/snapshot_votes_chainverse.sql
+++ data_gather/chainverse_db_queries/snapshot_votes_chainverse.sql
@@ -1,2 +1,6 @@
-SELECT *
-FROM snapshot.votes
+SELECT  sv.*,
+        sp.name space_name,
+		sp.about space_about,
+		sp.location space_location
+FROM snapshot.votes sv
+    INNER JOIN snapshot.spaces sp ON (sv.space_id = sp.id)
