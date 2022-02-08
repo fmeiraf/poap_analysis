a87834b0dec40081f11fcda392ece72b88d915d2
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2022-02-07 21:10:37 -0700

    added dao address and name to query

--- data_gather/chainverse_db_queries/dao_proposal_votes_daohaus.sql
+++ data_gather/chainverse_db_queries/dao_proposal_votes_daohaus.sql
@@ -1,8 +1,11 @@
 SELECT 	prop.*,
 		vt.name vote_type,
-		memb.address member_address
+		memb.address member_address,
+		dao.address dao_address,
+		dao.name dao_name
 FROM daohaus.proposal_votes prop
 	INNER JOIN daohaus.member memb ON (prop.member_id = memb.id)
+	INNER JOIN daohaus.dao dao ON (memb.dao_id = dao.id)
 	INNER JOIN daohaus.vote_type vt ON (vt.id = prop.vote_type_id)
 
 		
--- data_gather/chainverse_db_queries/dao_proposals_daohaus.sql
+++ data_gather/chainverse_db_queries/dao_proposals_daohaus.sql
@@ -1,5 +1,8 @@
-SELECT 	prop.*
+SELECT 	prop.*,
+		dao.name dao_name,
+		dao.address dao_address
 FROM daohaus.proposal prop
+	INNER JOIN daohaus.dao dao ON ( prop.dao_id = dao.id)
 WHERE 	prop.trade != true
 		AND prop.whitelist != true
 		AND prop."newMember" != true
